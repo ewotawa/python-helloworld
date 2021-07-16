34f62cbc63135a8c198c5f69d84cff0648012dfe
Elizabeth Wotawa <ewotawa@users.noreply.github.com>
2021-07-15 23:54:48 -0500

    update files to pull from solutions from lesson 1

--- app.py
+++ app.py
@@ -5,46 +5,35 @@ import logging
 app = Flask(__name__)
 
 @app.route('/status')
-def status():
-    ##define response
+def healthcheck():
     response = app.response_class(
-            response = json.dumps({'result': 'OK - healthy'}),
-            status = 200,
-            mimetype = 'application/json'
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
     )
 
-    # logging
-    app.logger.info('Status report successful')
-
-    # return
+    app.logger.info('Status request successfull')
     return response
 
 @app.route('/metrics')
 def metrics():
-    # response
     response = app.response_class(
-            response = json.dumps({'status': 'success', 'code': 0, 'data': {'UserCount': 140, 'UserCountActive': 23}}),
-            status = 200,
-            mimetype = 'application/json'
+            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+            status=200,
+            mimetype='application/json'
     )
 
-    # logging
-    app.logger.info('Metrics request successful')
-
-    # return
+    app.logger.info('Metrics request successfull')
     return response
 
-@app.route('/')
+@app.route("/")
 def hello():
-    # logging
-    app.logger.info('Main request successful')
+    app.logger.info('Main request successfull')
 
-    # return
     return "Hello World!"
 
-
 if __name__ == "__main__":
-    # stream logs to app.log file
-    logging.basicConfig(filename = 'app.log', level = logging.DEBUG)
+    ## stream logs to a file
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
     
     app.run(host='0.0.0.0')
